4931ee01a02ab2b093d6450244e446a5a00080f3
Yuval Dikerman <3dyuval@gmail.com>
2023-06-23 20:12:39 +0300

    past -> changing date

--- src/components/Feed.tsx
+++ src/components/Feed.tsx
@@ -1,20 +1,20 @@
 import { usePaginationFragment } from 'react-relay'
 import { graphql } from 'relay-runtime'
 import Link from './Link'
-import { useSearchParams, matchPath, useMatch } from 'react-router-dom'
+import { useSearchParams, useMatch } from 'react-router-dom'
 import { FeedLinksFragment$key } from './__generated__/FeedLinksFragment.graphql'
 import { useEffect } from 'react'
-
+import { sub, add } from 'date-fns'
 export const FeedLinksFragment = graphql`
   fragment FeedLinksFragment on Query
   @refetchable(queryName: "FeedLinksPaginationQuery")
   @argumentDefinitions(
     cursor: { type: "String" }
-    count: { type: "Int", defaultValue: 30 },
-    day: { type: "String", defaultValue: ""},
-    orderBy: {type: "String", defaultValue: "votes"}
+    count: { type: "Int", defaultValue: 30 }
+    date: { type: "String", defaultValue: null }
+    orderBy: { type: "String", defaultValue: null }
   ) {
-    feed(after: $cursor, first: $count, date: $day, orderBy: $orderBy)
+    feed(after: $cursor, first: $count, date: $date, orderBy: $orderBy)
       @connection(key: "FeedLinksFragment_feed") {
       pageInfo {
         hasNextPage
@@ -30,32 +30,73 @@ export const FeedLinksFragment = graphql`
 `
 
 export default function Feed({ feed }: { feed: FeedLinksFragment$key }) {
-  const [URLsearchParams] = useSearchParams()
-  const day = URLsearchParams.get('day')
+  const [URLsearchParams, setURLSearchParams] = useSearchParams()
+  const date = URLsearchParams.get('day')
   const orderByDate = useMatch('new')
+  const orderByComments = useMatch('comments')
 
-  const { data, loadNext, refetch } = usePaginationFragment(FeedLinksFragment, feed)
+  const { data, loadNext, refetch } = usePaginationFragment(
+    FeedLinksFragment,
+    feed
+  )
 
-  const {edges, pageInfo: { hasNextPage}} = data.feed
+  const {
+    edges,
+    pageInfo: { hasNextPage },
+  } = data.feed
   const onLoadMore = () => loadNext(30)
 
   useEffect(() => {
-    let orderBy = 'votes'
-    if (orderByDate) {
-      orderBy = 'date'
+    const refetchVariables = {}
+
+    if (date) {
+      Object.assign(refetchVariables, { date })
     }
-    refetch({day, orderBy})
-  }, [day, refetch, orderByDate])
+
+    if (orderByDate) {
+      Object.assign(refetchVariables, { orderBy: 'new' })
+    } else if (orderByComments) {
+      Object.assign(refetchVariables, { orderBy: 'comments' })
+    }
+    refetch(refetchVariables)
+  }, [date, refetch, orderByDate, orderByComments])
+
+  type Period = 'days' | 'months' | 'years'
+
+  const changeDate = {
+    add: (period: Period) => {
+      const result = add(new Date(date), { [period]: 1 })
+        .toISOString()
+        .split('T')[0]
+      setURLSearchParams({ day: result })
+    },
+    sub: (period: Period) => {
+      const result = sub(new Date(date), { [period]: 1 })
+        .toISOString()
+        .split('T')[0]
+      setURLSearchParams({ day: result })
+    },
+  }
 
   return (
     <>
+      {date && (
+        <div className="text-zinc-600 text-md p-4">
+          <p> Stories from {date} </p>
+          <p>
+            Go back a <a role="button" sonClick={() => changeDate.sub('days')}>day</a>,{' '}
+            <a role="button" onClick={() => changeDate.sub('months')}>month</a>, or{' '}
+            <a role="button" onClick={() => changeDate.sub('years')}>year</a>. Go forward a{' '}
+            <a role="button" onClick={() => changeDate.add('days')}>day</a> or{' '}
+            <a role="button" onClick={() => changeDate.add('months')}>month</a>.
+          </p>
+        </div>
+      )}
       {edges &&
         edges.map(({ node, cursor }) => (
           <Link link={node} key={cursor} cursor={cursor} />
         ))}
-      {hasNextPage && (
-        <button onClick={onLoadMore}> Load more... </button>
-      )}
+      {hasNextPage && <button onClick={onLoadMore}> Load more... </button>}
     </>
   )
 }
